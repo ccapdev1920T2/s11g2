9c319b51e4bb9c0d0dafbf813ce82a148dfb4c75
Gridmerc <58649232+Gridmerc@users.noreply.github.com>
2020-04-01 19:28:01 +0800

    Express + HBS

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
-/* Importing Modules */
+/*
+    LAST EDITED: APRIL 1, 2020
+    CCAPDEV S11 GROUP 2
+    VIRTUS ET SCIENTIA
+*/
+
+/* Import Modules Here */
 const express = require('express');
+const hbs = require('hbs');
 
-/* Express */
+/* Port */
 const app = express();
-const port = 3030;
+const port = 3000;
 
-/* Default Path */
+/* View Engine */
+app.set('view engine', 'hbs');
+
+/* Routing */
+
+/* HOME PAGE */
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+    res.render('home');
 });
 
-app.get('/homepage', function(req, res) {
-	res.sendFile(__dirname + "/static/index.html");
+/* USER PAGE */
+app.get('/user', function(req, res) {
+    res.render('user');
 });
 
-/* Static File Function */
-app.use('static', express.static('files'));
+/* BUILD PAGE */
+app.get('/build', function(req, res) {
+    res.render('build');
+});
 
-/* 404 Error Function */
+/* CONTACT PAGE */
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+/* REGISTER PAGE */
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+/* SEARCH PAGE */
+app.get('/search', function(req, res) {
+    res.render('search');
+});
+
+/* Functions */
+
+/* ACCESS STATIC FILES */
+app.use(express.static('public'));
+
+/* 404 FILE NOT FOUND */
 app.use(function(req, res, next) {
-	res.status(404).send("The file is not in server!");
+    res.status(404).send('File is not in the server!');
 });
 
-/* Listening Port */
+/* Listeners */
 app.listen(port, function() {
-	console.log('App listening at port ' + port);
+    console.log('App listening at port ' + port);
 });
