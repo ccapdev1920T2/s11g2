9c2f5b6493cc8d6c95020027a9077db065fbf518
rdj519 <39096856+rdj519@users.noreply.github.com>
2020-04-25 20:32:59 +0800

    Add files via upload

--- helpers/validation.js
+++ helpers/validation.js
@@ -32,6 +32,54 @@ const validation = {
 
         return validation;
     }
+
+    addCarValidation: function(){
+        var validation = [
+            check('name', 'Car name should be atleast three characters.').isLength({min:3}),
+
+            check('brand', 'Brand name should be atleast three characters.').isLength({min:3}),
+
+            check('carClass', 'Car class should not be empty.').notEmpty(),
+
+            check('bodyStyle', 'Car body style should be atleast three characters.').isLength({min:3}),
+
+            check('capacity', 'Invalid input.').notEmpty().isDecimal(),
+
+            check('price', 'Invalid price').notEmpty().isDecimal(),
+
+            check('description', 'Description should be atleast three characters.').isLength({min:3}),
+
+            check('imgLink', 'Image link should be atleast three characters.').isLength({min:3}),
+
+            check('optionImg', 'Option Image link should be atleast three characters.').isLength({min:3})
+        ];
+
+        return validation;
+    }
+
+    updateCarValidation: function(){
+        var validation = [
+            check('name', 'Car name should be atleast three characters.').isLength({min:3}),
+
+            check('brand', 'Brand name should be atleast three characters.').isLength({min:3}),
+
+            check('carClass', 'Car class should not be empty.').notEmpty(),
+
+            check('bodyStyle', 'Car body style should be atleast three characters.').isLength({min:3}),
+
+            check('capacity', 'Invalid input.').notEmpty().isDecimal(),
+
+            check('price', 'Invalid price').notEmpty().isDecimal(),
+
+            check('description', 'Description should be atleast three characters.').isLength({min:3}),
+
+            check('imgLink', 'Image link should be atleast three characters.').isLength({min:3}),
+            
+            check('optionImg', 'Option Image link should be atleast three characters.').isLength({min:3})
+        ];
+
+        return validation;
+    }
 }
 
 module.exports = validation;
